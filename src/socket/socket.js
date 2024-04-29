bd67f9eb4f58c1a9e3ae8cf7b5874a4cd63673f9
Yashwant937363 <yashwantpoyrekar@gmail.com>
2024-04-29 21:15:34 +0530

    going for deploy

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const socket = io("http://localhost:5000", { autoConnect: false });
+const socket = io(process.env.REACT_APP_SERVER_URL, { autoConnect: false });
 export default socket;
 
 export function startConnection() {
